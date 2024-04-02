db1391b093865d28ab43cf39a8591989432bf56e
LeangHuy <120023658+LeangHuy@users.noreply.github.com>
2024-04-02 22:32:10 +0700

    first commit

---
+++ src/main/java/org/example/demorestapi2/DemoRestApi2Application.java
@@ -0,0 +1,13 @@
+package org.example.demorestapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/demorestapi2/DemoRestApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demorestapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
